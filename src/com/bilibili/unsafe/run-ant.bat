340e74e1fc815098a0b02480d820743141e4965f
xiaoping.tian <xiaoping.tian@thermofisher.com>
2023-08-08 17:53:38 +0800

    update run-ant.bat

--- src/com/bilibili/unsafe/run-ant.bat
+++ src/com/bilibili/unsafe/run-ant.bat
@@ -1,9 +1,9 @@
 @ECHO OFF
 setlocal
 
-set JAVA_HOME=%~dp0..\..\..\Tools\jdk1.8.0_232
+set JAVA_HOME=%~dp0Tools\jdk1.8.0_232
 set JAVA_OPTS=-Xms512m -Xmx1024m
-set ANT_HOME=%~dp0..\..\..\Tools\apache-ant-1.7.0
+set ANT_HOME=%~dp0Tools\apache-ant-1.7.0
 set ANT_OPTS=-Xms512m -Xmx1024m
 set PATH=%PATH%;%ANT_HOME%\bin;%JAVA_HOME%\bin
 
